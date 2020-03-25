2ba9ea5b9a34e3adaca0b14b7d3a28451539b757
kaani65 <kaan65md@hotmail.de>
2020-03-25 13:53:43 +0100

    integer fail

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def aws_query():
             Granularity="MONTHLY",
             Metric="BLENDED_COST"
         )
-        forecast = r["Total"]["Amount"]["ForecastResultsByTime"]
+        forecast = r["Total"][0]["Amount"]["ForecastResultsByTime"]
         print("Updated AWS Forecast cost: %s" % (forecast))
         g_forecast.set(float(forecast))
 
