6bcc9d71a27032f6e4669934f1daa0e8a09bf5f2
kaani65 <kaan65md@hotmail.de>
2020-04-13 18:23:58 +0200

    change

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def aws_query():
             Granularity="MONTHLY",
             Metric="BLENDED_COST"
         )
-        forecast = r["Total"]["Amount"]["ForecastResultsByTime"]["BLENDED_COST"]
+        forecast = r["Total"]["Amount"]["BLENDED_COST"]
         print("Updated AWS Forecast cost: %s" %(forecast))
         g_forecast.set(float(forecast))
 
