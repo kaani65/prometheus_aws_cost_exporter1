6163c1e24ff0935aab7a81e30a534d3c5b0aa882
kaani65 <kaan65md@hotmail.de>
2020-04-15 00:39:41 +0200

    0 cahnge

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def aws_query():
             Granularity="MONTHLY",
             Metric="NET_UNBLENDED_COST"
         )
-        forecast = r["Total"]["Amount"]
+        forecast = r["Total"]["Amount"][0]
         print("Updated AWS Forecast cost: %s" %(forecast))
         g_forecast.set(float(forecast))
 
